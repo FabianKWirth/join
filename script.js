f80df9071f4003a7b30c35b097456e972bb7d0d7
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-08-24 19:05:52 +0200

    added a description for the includeHTML() Function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 ///************INCLUDE HTML-TEMPLATES*************///
 
+/**
+ * Searches for the "w3-include-html" attribute in the HTML file 
+ * and replaces it with the value of this attribute
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
