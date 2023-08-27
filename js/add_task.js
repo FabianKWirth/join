79521746f9f38e1c5b5d988de9ff9471c78232bb
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-08-28 01:47:25 +0200

    improved add_task form

--- js/add_task.js
+++ js/add_task.js
@@ -1,54 +1,100 @@
-let selectedPriority = null //Priorities= urgent, medium, low
+let selectedTaskPriority = null //Priorities= urgent, medium, low
+let showAssignedUsers=false;
 
+users = [
+    {
+        "username": "john_doe",
+        "password": "securePassword123",
+        "email": "john.doe@example.com"
+    },
+    {
+        "username": "jane_smith",
+        "password": "strongPass456",
+        "email": "jane.smith@example.com"
+    },
+    {
+        "username": "user123",
+        "password": "password789",
+        "email": "user123@example.com"
+    }
+];
 
 renderTaskInput();
 
-
-
-function renderTaskInput(){
+function renderTaskInput() {
     setAssignedUserData();
     setTaskData();
 }
+
+
 function collectTaskData() {
-
     const container = document.getElementById('addTaskContainer');
-
     const task = {};
-    console.log(container);
 
-    const taskId=createTaskId();
-    const titleInput = container.querySelector('#newTaskTitle');
-    const descriptionTextarea = container.querySelector('#newTaskDescription');
-    const assignedUserSelect = container.querySelector('#newTaskAssignedUser');
-    const dueDateInput = container.querySelector('#newTaskDate');
-    const priorityButtons = container.querySelectorAll('.priority-button');
-    const categorySelect = container.querySelector('#newTaskCategory');
-    const subTaskSelect = container.querySelector('#newTaskSubTask');
+    const titleInput = container.querySelector('#newTaskTitle').value;
+    const descriptionTextarea = container.querySelector('#newTaskDescription').value;
+    const assignedUsers = container.querySelector('assignedUserList').value;
+    const dueDateInput = container.querySelector('#newTaskDate').value;
+    const categorySelect = container.querySelector('#newTaskCategory').value;
+    const subTaskSelect = container.querySelector('#newTaskSubTask').value;
 
-    task.taskId=taskId;
-    task.title = titleInput.value;
-    task.description = descriptionTextarea.value;
-    task.assignedUser = assignedUserSelect.value;
-    task.dueDate = dueDateInput.value;
+    if (titleInput != "" & assignedUserSelect != "" & dueDateInput != "" & categorySelect != "" & selectedTaskPriority != null) {
+        if (checkIfTaskWithThisTitleExists(titleInput) == false) {
+            task.title = titleInput;
+            task.description = descriptionTextarea;
+            task.assignedUser = assignedUserSelect;
+            task.dueDate = dueDateInput;
+            task.priority = selectedTaskPriority;
+            task.category = categorySelect;
+            task.subTask = subTaskSelect;
 
-    priorityButtons.forEach(button => {
-        if (button.classList.contains('selected')) {
-            task.priority = button.textContent.trim();
+            setCreateTaskStatus("Enabled");
+            return task;
+        } else {
+            alert("A Task with this Name Already Exists");
         }
-    });
+    } else {
+        setCreateTaskStatus("Disabled");
+        console.log("Values are missing");
+    }
+}
 
-    task.category = categorySelect.value;
-    task.subTask = subTaskSelect.value;
-    console.log(task);
-    return task;
+function setCreateTaskStatus(status) {
+    if (status == "Disabled") {
+        document.getElementById("createTask").disabled = true;
+    } else {
+        document.getElementById("createTask").disabled = false;
+    }
+}
+
+
+/**
+ * Checks if a task with the specified title exists in the tasks array.
+ *
+ * @param {string} titleInput - The title to check for in the tasks array.
+ * @param {Array<{title: string}>} tasks - An array of task objects containing 'title' property.
+ * @returns {boolean} True if a task with the given title exists, otherwise false.
+ */
+function checkIfTaskWithThisTitleExists(titleInput) {
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(titleInput);
+        console.log(tasks[i]['title']);
+        if (titleInput == tasks[i]['title']) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function createTask() {
     task = collectTaskData();
-    tasks.push(task);
-    setItem("tasks", tasks);
+    if (task != false) {
+        tasks.push(task);
+        setItem("tasks", tasks);
+    } else {
+        alert("A task with this name already exists");
+    }
 }
-
 
 
 /**
@@ -58,17 +104,44 @@ function createTask() {
  * for task assignment. Each user's email and username are used as option values and
  * text respectively.
  */
-function setAssignedUserData(){
+function setAssignedUserData() {
     /**
      * @type {HTMLElement}
      */
     let dropDownElement = document.getElementById("newTaskAssignedUser");
+    const userForm = document.getElementById('assignUserFrom');
+    const userList = document.getElementById('assignedUserList');
+
     for (let i = 0; i < users.length; i++) {
-        const userToSelect = users[i];
-        dropDownElement.innerHTML +=/*html*/`
-        <option value='${userToSelect['email']}'>${userToSelect['username']}</option>
-        `
+        const user = users[i];
+
+        let userName = user['username'];
+        //let userIcon=getUserIcon(user['username']);
+        let userIcon = "";
+        let userMail = user['email'];
+
+        userList.innerHTML +=/*html*/`
+        <tr onclick='setUserSelected(this)' class id='userId' value='userMail'>
+            <td>${userIcon}</td>
+            <td>${userName}</td>
+            <td><input type='checkBox' id='selectedUserCheckBox${userMail}'></td>
+        </tr>`;
     }
+
+        // Do something with the selected usernames
+        //console.log('Selected usernames:', selectedUsernames);
+
+}
+
+function changeAvailableUsersVisibility(){
+    if(showAssignedUsers==false){
+        showAssignedUsers=true;
+        document.getElementById("dropDownContent").classList.remove("hide");
+    }else{
+        showAssignedUsers=false;
+        document.getElementById("dropDownContent").classList.add("hide");
+    }
+   
 }
 
 /**
@@ -117,10 +190,28 @@ function setButtonSelected(currentButton) {
          */
         const button = buttons[i];
         if (button == currentButton) {
-            selectedPriority = button.value;
-            button.classList.add("priority-button-selected");
+            setButtonSelectStatusById(button.value, true);
         } else {
-            button.classList.remove("priority-button-selected");
+            setButtonSelectStatusById(button.value, false);
         }
     }
+}
+
+
+
+function setButtonSelectStatusById(type, selectStatus = false) {
+    console.log(`priority-button-${type}-selected`, selectStatus);
+    let button = document.querySelector('#newTaskCategory');
+
+    if (selectStatus == true) {
+        document.getElementById(`${type}Button`).classList.add(`priority-button-${type}-selected`);
+        document.getElementById(`${type}Icon`).classList.add('white-symbol');
+    } else {
+        document.getElementById(`${type}Button`).classList.remove(`priority-button-${type}-selected`);
+        document.getElementById(`${type}Icon`).classList.remove('white-symbol');
+    }
+}
+
+function emptyAddTaskForm() {
+
 }
