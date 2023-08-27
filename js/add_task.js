9927d48c22a0c0500b7c85475313ccaed2242181
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-08-27 22:03:03 +0200

    updated add_task_functionality

--- js/add_task.js
+++ js/add_task.js
@@ -1,11 +1,22 @@
 let selectedPriority = null //Priorities= urgent, medium, low
 
+
+renderTaskInput();
+
+
+
+function renderTaskInput(){
+    setAssignedUserData();
+    setTaskData();
+}
 function collectTaskData() {
 
     const container = document.getElementById('addTaskContainer');
 
     const task = {};
     console.log(container);
+
+    const taskId=createTaskId();
     const titleInput = container.querySelector('#newTaskTitle');
     const descriptionTextarea = container.querySelector('#newTaskDescription');
     const assignedUserSelect = container.querySelector('#newTaskAssignedUser');
@@ -14,6 +25,7 @@ function collectTaskData() {
     const categorySelect = container.querySelector('#newTaskCategory');
     const subTaskSelect = container.querySelector('#newTaskSubTask');
 
+    task.taskId=taskId;
     task.title = titleInput.value;
     task.description = descriptionTextarea.value;
     task.assignedUser = assignedUserSelect.value;
@@ -34,18 +46,81 @@ function collectTaskData() {
 function createTask() {
     task = collectTaskData();
     tasks.push(task);
-    setItem("tasks", tasks)
+    setItem("tasks", tasks);
 }
 
 
-function setTaskData() {
-    let dropDownElement = document.getElementById("newTaskSubTask");
-    for (let i = 0; i < tasks.length; i++) {
-        const taskToSelect = task[i];
-        dropDownElement.innerHTML=/*html*/`
-        <option id='${taskToSelect[id]}'>${taskToSelect['title']}</option>
+
+/**
+ * Populates a dropdown element with user data for task assignment.
+ *
+ * This function populates a given dropdown element with user data to provide options
+ * for task assignment. Each user's email and username are used as option values and
+ * text respectively.
+ */
+function setAssignedUserData(){
+    /**
+     * @type {HTMLElement}
+     */
+    let dropDownElement = document.getElementById("newTaskAssignedUser");
+    for (let i = 0; i < users.length; i++) {
+        const userToSelect = users[i];
+        dropDownElement.innerHTML +=/*html*/`
+        <option value='${userToSelect['email']}'>${userToSelect['username']}</option>
         `
-        dropDownOption.innerHTML = tasks[i]['title'];
-        dropDownOption.index = "Subtask".tasks[i]['id'];
+    }
+}
+
+/**
+ * Populates a dropdown element with task data for subtask assignment.
+ *
+ * This function populates a given dropdown element with task data to provide options
+ * for subtask assignment. Each task's ID and title are used as option values and text
+ * respectively.
+ */
+function setTaskData() {
+    /**
+     * @type {HTMLElement}
+     */
+    let dropDownElement = document.getElementById("newTaskSubTask");
+
+    for (let i = 0; i < tasks.length; i++) {
+        /**
+         * @type {Object}
+         */
+        const taskToSelect = tasks[i];
+
+        dropDownElement.innerHTML += /*html*/ `
+        <option value='${taskToSelect['id']}'>${taskToSelect['title']}</option>
+        `;
+    }
+}
+
+/**
+ * Sets the selected state of a button and updates the associated priority value.
+ *
+ * This function sets the selected state for a button among a group of buttons
+ * based on the provided `currentButton`. The selected button's appearance is
+ * updated to indicate its selection status, and the associated priority value
+ * is updated accordingly.
+ *
+ * @param {HTMLButtonElement} currentButton - The button to be set as selected.
+ */
+function setButtonSelected(currentButton) {
+    /**
+     * @type {HTMLCollectionOf<HTMLButtonElement>}
+     */
+    let buttons = document.getElementById("newTaskPriority").getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+        /**
+         * @type {HTMLButtonElement}
+         */
+        const button = buttons[i];
+        if (button == currentButton) {
+            selectedPriority = button.value;
+            button.classList.add("priority-button-selected");
+        } else {
+            button.classList.remove("priority-button-selected");
+        }
     }
 }
