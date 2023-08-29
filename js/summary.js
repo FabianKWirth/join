45f030c88dc4edb63f27d08c40fc8942544bd564
HuberPascal <pascal.huber92@gmx.ch>
2023-08-29 20:01:07 +0200

    header initials

--- js/summary.js
+++ js/summary.js
@@ -1,45 +1,3 @@
-/**
- * This function marks the menu item you are on
- * 
- * @param {number} x - That is the menu item to be loaded
- */
-function bgDark(x) {
-    if (x < 4) {
-        document.getElementById(`menu-link${x}`).classList.add('bg-dark', 'white');
-        document.getElementById(`menu-img${x}`).src = `assets/image/sidebar/menu-${x}-white.svg`;
-    };
-}
-
-/**
- * This function highlights the menu item under Legal Notice that you are currently on
- * 
- * @param {number} x - That is the menu item to be loaded
- */
-function bgDarkLegalNotice(x) {
-    if (x > 3) {
-        document.getElementById(`menu-link${x}`).classList.add('bg-dark-legal-notice');
-        document.getElementById('sidebar-menu').classList.add('dNone');
-    }
-}
-
-// data must still be loaded from the server
-/**
- * This function loads the account data from the server
- */
-function setInitials() {
-    document.getElementById('myAccount').innerHTML = 'PH';
-}
-
-/**
- * This function goes back to the menu
- */
-function goBack() {
-    document.getElementById('sidebar-menu').classList.remove('dNone');
-    document.getElementById(`menu-link4`).classList.remove('bg-dark-legal-notice');
-    document.getElementById(`menu-link5`).classList.remove('bg-dark-legal-notice');
-    document.getElementById('goBack').classList.add('dNone');
-}
-
 
 function greetUser() {
     greetingTime();
@@ -71,13 +29,6 @@ function greetingUser() {
     // console.log(user);
 }
 
-
-/**
- * This function opens the menu from the header
- */
-function ShowMenu() {
-    document.getElementById('header-menu').classList.toggle('dNone');
-}
 
 function hoverRightButton() {
     let circle = document.getElementById('button-right-hover1');
--- script.js
+++ script.js
@@ -135,3 +135,62 @@ function getContactIconHtml(user) {
 }
 
 
+/**
+ * This function marks the menu item you are on
+ * 
+ * @param {number} x - That is the menu item to be loaded
+ */
+function bgDark(x) {
+    if (x < 4) {
+        document.getElementById(`menu-link${x}`).classList.add('bg-dark', 'white');
+        document.getElementById(`menu-img${x}`).src = `assets/image/sidebar/menu-${x}-white.svg`;
+    };
+}
+
+/**
+ * This function highlights the menu item under Legal Notice that you are currently on
+ * 
+ * @param {number} x - That is the menu item to be loaded
+ */
+function bgDarkLegalNotice(x) {
+    if (x > 3) {
+        document.getElementById(`menu-link${x}`).classList.add('bg-dark-legal-notice');
+        document.getElementById('sidebar-menu').classList.add('dNone');
+    }
+}
+
+// data must still be loaded from the server
+/**
+ * This function loads the account data from the server
+ */
+function setInitials() {
+    let myAccount = document.getElementById('myAccount');
+    let user = users[0]['username'];
+    let name = user.split("_");
+    // let name = user.split(" ");
+    console.log(name);
+    let firstName = name[0].charAt(0).toUpperCase();
+    let lastName = name[1].charAt(0).toUpperCase();
+    let initials = firstName + lastName;
+    console.log(firstName)
+    console.log(lastName)
+    myAccount.innerHTML = initials;
+}
+
+/**
+ * This function goes back to the menu
+ */
+function goBack() {
+    document.getElementById('sidebar-menu').classList.remove('dNone');
+    document.getElementById(`menu-link4`).classList.remove('bg-dark-legal-notice');
+    document.getElementById(`menu-link5`).classList.remove('bg-dark-legal-notice');
+    document.getElementById('goBack').classList.add('dNone');
+}
+
+
+/**
+ * This function opens the menu from the header
+ */
+function ShowMenu() {
+    document.getElementById('header-menu').classList.toggle('dNone');
+}
