b4f375c8917ab6194af3f1a20cafc7867903cdc7
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-09-03 15:23:45 +0200

    legal notice responsiv

--- js/legal_notice.js
+++ js/legal_notice.js
@@ -3,8 +3,10 @@
  */
 function disableIcon() {
   let container = document.getElementById("header-icon");
+  let container2 = document.getElementById('myAccount-responsive')
   if (window.location.href == 'http://127.0.0.1:5501/legal-notice.html' || window.location.href == 'http://gruppe-671.developerakademie.net/join/legal-notice.html') {
     container.classList.add("d-none");
+    container2.classList.add("d-none");
   }
 }
 
