7709871f37e08627dd4e9b680344370fa125620b
HuberPascal <pascal.huber92@gmx.ch>
2023-09-08 22:42:52 +0200

    board filter

--- js/board.js
+++ js/board.js
@@ -288,8 +288,7 @@ function filterToDo() {
     let list = document.getElementById('todo');
     list.innerHTML = '';
 
-    let todo = task.filter(t => t['category'] == 'Todo');
-
+    let todo = tasks.filter(t => t['status'] == 'toDo');
     renderSearchListToDo(todo, list, searchInput);
 }
 
@@ -309,7 +308,7 @@ function renderSearchListToDo(todo, list, searchInput) {
     if (todo.length > 0) {
         for (let i = 0; i < todo.length; i++) {
             let element = todo[i];
-            if (element['text'].toLowerCase().includes(searchInput)) {
+            if (element['taskName'].toLowerCase().includes(searchInput)) {
                 list.innerHTML += generateHTML(element);
                 searchElementsFound = true;
             }
@@ -333,7 +332,7 @@ function filterInProgress() {
     let list = document.getElementById('inProgress');
     list.innerHTML = '';
 
-    let progress = task.filter(t => t['category'] == 'inProgress')
+    let progress = tasks.filter(t => t['status'] == 'inProgress')
 
     renderSearchListInProgress(progress, list, searchInput)
 }
@@ -353,7 +352,7 @@ function renderSearchListInProgress(progress, list, searchInput) {
     if (progress.length > 0) {
         for (let i = 0; i < progress.length; i++) {
             let element = progress[i];
-            if (element['text'].toLowerCase().includes(searchInput)) {
+            if (element['taskName'].toLowerCase().includes(searchInput)) {
                 list.innerHTML += generateHTML(element);
                 searchElementsFound = true;
             }
@@ -377,7 +376,7 @@ function filterAwaitFeedback() {
     let list = document.getElementById('awaitFeedback');
     list.innerHTML = '';
 
-    let feedback = task.filter(t => t['category'] == 'awaitFeedback');
+    let feedback = tasks.filter(t => t['status'] == 'awaitFeedback');
     renderSearchListAwaitFeedback(feedback, list, searchInput);
 }
 
@@ -397,7 +396,7 @@ function renderSearchListAwaitFeedback(feedback, list, searchInput) {
     if (feedback.length > 0) {
         for (let i = 0; i < feedback.length; i++) {
             let element = feedback[i];
-            if (element['text'].toLowerCase().includes(searchInput)) {
+            if (element['taskName'].toLowerCase().includes(searchInput)) {
                 list.innerHTML += generateHTML(element);
                 searchElementsFound = true;
             }
@@ -421,7 +420,7 @@ function filterDone() {
     let list = document.getElementById('done');
     list.innerHTML = '';
 
-    let done = task.filter(t => t['category'] == 'Done');
+    let done = tasks.filter(t => t['status'] == 'done');
     renderSearchListDone(done, list, searchInput);
 }
 
@@ -441,7 +440,7 @@ function renderSearchListDone(done, list, searchInput) {
     if (done.length > 0) {
         for (let i = 0; i < done.length; i++) {
             let element = done[i];
-            if (element['text'].toLowerCase().includes(searchInput)) {
+            if (element['taskName'].toLowerCase().includes(searchInput)) {
                 list.innerHTML += generateHTML(element);
                 searchElementsFound = true;
             }
@@ -567,7 +566,7 @@ function subtaskChangeImg(id) {
         subtaskField.src = 'assets/image/board/Check-button-empty.svg';
     } else {
         subtaskField.src = 'assets/image/board/Check-button.svg';
-    }
+    }  
 }
 
 function closeBoardOverlay() {
@@ -613,25 +612,59 @@ function assignedInicials(index) {
 }
 
 
-function getSubtasks(subtasks) {
+function getSubtasks(subtasks, task) {
     let subtasksHTML = '';
-   
+    
+    console.log('geht task?', subtasks)
+    
+
+
     if (subtasks) {
         for (let i = 0; i < subtasks.length; i++) {
             const subtask = subtasks[i];
             const subtaskId = `subtask${i + 1}`;
+            console.log('geht task wirklich?', subtasks[i]['subtaskStatus'])
+            let subtaskStatus = subtasks[i]['subtaskStatus'];
+            // let subtaskStatusImg = subtaskStatus(subtaskStatus);
 
-                subtasksHTML += `
-                    <div class="subtask">
-                        <img id="${subtaskId}" onclick="subtaskChangeImg('${subtaskId}', ${i}), saveSubtask(${i})" src="assets/icons/checkbox-empty.svg">
-                        <div>${subtask['name']}</div>
-                    </div>
-                    `;             
+            subtasksHTML += `
+                <div class="subtask">
+                    <img id="${subtaskId}" onclick="subtaskChangeImg('${subtaskId}', ${i}); saveSubtask(${subtaskStatus}, ${i})" src="assets/icons/checkbox-empty.svg">
+                    <div>${subtask['name']}</div>
+                </div>
+            `;
         }
     }
     return subtasksHTML;
 }
 
+// assets/icons/checkbox-empty.svg
+
+function subtaskStatus(i) {
+    alert(i)
+    // if tasks[i]['subTasks'][0]['subtaskStatus']
+}
+
+
+
+
+async function saveSubtask(subtaskStatus, i) {
+    console.log('der task status ist', subtaskStatus)
+    subtaskStatus = true;
+
+    // console.log('hallo', tasks[11]['taskName'])
+
+    // alert(tasks[i]['subTasks'][0]['subtaskStatus'])
+    tasks[i]['subTasks'][0]['subtaskStatus'] = subtaskStatus;
+    await setItem('tasks', tasks);
+    console.log(tasks);
+
+    console.log('task zweitens', subtaskStatus)
+
+
+
+    // console.log('subtask hat den status', task['subTasks'][0]);
+}
 
 
 
@@ -713,85 +746,3 @@ async function openEditTaskTemplate(taskId) {
     includeEventlistenerToCloseAddTask()
 }
 
-
-
-
-
-
-
-
-// function showTaskCard(index) {
-//     let task = tasks[index];
-//     const category = task['taskCategoryValue'];
-//     const name = task['taskName'];
-//     const description = task['taskDescription'];
-//     const date = task['taskDate'];
-//     const priority = task['priority'];
-//     let assignedContactsHTML = getAssignedContacts(task['assignedContacts']);
-//     console.log(assignedContactsHTML);
-//     const initials = [assignedInicials(0), assignedInicials(1), assignedInicials(2)];
-//     const assignedNames = [assignedTo(0), assignedTo(1), assignedTo(2)];
-//     let subtasksHTML = getSubtasks(task['subTasks'], task);
-//     const subtask1 = task['subTasks'] && task['subTasks'][0] ? task['subTasks'][0]['name'] : '';
-//     const subtask2 = task['subTasks'] && task['subTasks'][1] ? task['subTasks'][1]['name'] : '';
-//     let assignedName = '';
-
-//     let showTaskCard = document.getElementById('showTaskCard');
-//     console.log('prioritiy ist', priority)
-//     let priorityImageSrc = getPriorityImageSrc(priority);
-
-//     showTaskCard.innerHTML = `
-//         <div id="overlayBoard" class="overlayBoard">
-//             <div class="task-card-overlay">
-//                 <div class="card-category-top-section">
-//                     <div class="card-category-overlay">${category}</div> 
-//                     <img onclick="closeBoardOverlay()" src="assets/icons/close.svg">
-//                 </div>
-//                 <div>
-//                     <h4 class="title-h4">${name}</h4>
-//                 </div>
-//                 <div class="card-description-overlay">${description}</div>
-//                 <div  class="card-description-overlay">
-//                     <div class="dark-gray">Due date:</div>
-//                     <div>${date}</div>
-//                 </div>
-//                 <div class="priority-container card-description-overlay">
-//                     <div class="dark-gray">Priority:</div>
-//                     <div class="priority">
-//                         <div>${priority}</div>
-//                         <img src="${priorityImageSrc}">
-//                     </div>
-//                 </div>
-//                 <div class="assigned">
-//                     <div>
-//                         <div class="dark-gray">Assigned To:</div>
-//                     </div>
-//                     <div>
-//                         ${assignedContactsHTML}
-//                     </div>
-//                 </div>
-//                 <div class="subtasks">
-//                     <div>
-//                         <div class="dark-gray">Subtask</div>
-//                     </div>
-//                     <div class="subtasks-container">
-//                         ${subtasksHTML}
-//                     </div>
-//                     </div>
-//                 </div>
-//                 <div class="edit-container">
-//                     <div class="edit" onmouseover="changeImage('assets/icons/delete-blue.svg', 'trashImage')" onmouseout="changeImage('assets/icons/trashcan-icon.svg', 'trashImage')">
-//                         <img id="trashImage" src="assets/icons/trashcan-icon.svg">
-//                         <div>Delete</div>
-//                     </div>
-//                     <img src="assets/image/board/edit-line.svg">
-//                     <div class="edit" onmouseover="changeImage('assets/icons/edit-blue.svg', 'editImage')" onmouseout="changeImage('assets/icons/pen-icon.svg', 'editImage')">
-//                         <img id="editImage" src="assets/icons/pen-icon.svg"> 
-//                         <div onclick="openEditTaskTemplate(1)">Edit</div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     `;
-//     includeEventlistenerToCloseOverlayBoard();
-// }
